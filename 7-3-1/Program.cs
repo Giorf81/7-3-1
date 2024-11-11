c055c39f6e91867fef491c0cee8c28c3c956ee4d
Giorf81 <rejroberts7@gmail.com>
2024-11-11 23:10:10 +0300

    Добавьте файлы проекта.

---
+++ 7-3-1/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _7_3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ComputerPart part = new Processor();
+
+            part.Work();
+           
+        }
+    }
+
+    abstract class ComputerPart
+    {
+        public abstract void Work();
+    }
+
+    class Processor : ComputerPart
+    {
+        public override void Work()
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("я процессор мне 2.4Ггц");
+        }
+    }
+
+    class MotherBoard : ComputerPart
+    {
+        public override void Work()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("я мать твоя");
+        }
+    }
+
+    class GraphicCard : ComputerPart
+    {
+        public override void Work()
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("жужу я делаю фпсы в играх жужу");
+        }
+    }
+
+}
